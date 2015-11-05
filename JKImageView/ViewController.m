41784b37cee7680b2a7f05abf5f697a8b55551b0
Jack <erfeng.cheng@dianping.com>
2015-11-05 18:48:22 +0800

    ProjectInit

---
+++ JKImageView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JKImageView
+//
+//  Created by Jack on 15/11/5.
+//  Copyright © 2015年 Jack. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
